13abbcf8c0ca82bc9a788c74da8283338394ae0c
Chris Goodings <christopher.n.goodings@student.shu.ac.uk>
2026-08-16 11:44:31 +0100

    Configure HealerLab API for Cloudflare Workers

---
+++ worker.js
@@ -0,0 +1,31 @@
+﻿import { onRequestGet as getCharacter } from './functions/api/character.js';
+
+function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      'cache-control': 'no-store',
+    },
+  });
+}
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/api/character') {
+      if (request.method !== 'GET') {
+        return json({ message: 'Method not allowed.' }, 405);
+      }
+
+      return getCharacter({ request, env });
+    }
+
+    if (url.pathname.startsWith('/api/')) {
+      return json({ message: 'API route not found.' }, 404);
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
